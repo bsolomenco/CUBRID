047103a388cfb927f235dfd7792dc8a0e7731c0c
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2017-11-08 09:40:54 +0200

    Windows shel script (.bat) to generate 'Visual Studio 15 2017 Win64' solution

---
+++ g.bat
@@ -0,0 +1,9 @@
+@set dt0=%DATE% %TIME%
+@echo %dt0%
+rmdir /S /Q ..\build  &  mkdir ..\build & pushd ..\build
+cmake -G "Visual Studio 15 2017 Win64" -DUNIT_TESTS=ON ..\repo
+popd
+@set dt1=%DATE% %TIME%
+@echo ----==== dt1-dt0 ====----
+@echo %dt0%
+@echo %dt1%
